42938e72c50dd14f4839599076c9011f7b48557f
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2020-09-15 00:27:38 -0400

    Week 4
    Higher order functions
    Object oriented programming
    Error handling

---
+++ Week4/error-handling.js
@@ -0,0 +1,16 @@
+function validate(num) {
+    if (typeof (num) !== 'number') {
+        throw new TypeError(`The value ${num} is not a number`);
+    }
+    console.log(`The value ${num} is valid.`);
+}
+
+const numbers = [1, 2, 'banana', 4, 'orange', 5];
+
+numbers.forEach(function (val) {
+    try {
+        validate(val);
+    } catch (error) {
+        console.log(error.message);
+    }
+});
