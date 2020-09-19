064cacdb37a359fb3ae5afb5007f4b67b9fa6f89
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2020-09-18 23:29:35 -0400

    Arrow Functions and Modern Array Functions
    Implentation of arrow functions and modern array functions

--- Week5/Project1/project1.js
+++ Week5/Project1/project1.js
@@ -2,17 +2,60 @@ const json = require('./siege.json') //Get the JSON file
 
 //Create a higher order function that takes in a sort function and iterates over the array
 
+class Player {
+    constructor(name, platform, rank, kd) {
+        this.name = name;
+        this.platform = platform;
+        this.rank = rank;
+        this.kd = kd;
+    }
 
+    get name() {
+        return this.name;
+    };
+
+    set name(name) {
+        this.name = name;
+    }
+
+    get platform() {
+        return this.platform;
+    };
+
+    set platform(platform) {
+        this.platform = platform;
+    };
+
+    get rank() {
+        return this.rank;
+    };
+
+    set rank(rank) {
+        this.rank = rank;
+    };
+
+    get kd() {
+        return this.kd;
+    };
+
+    set kd(kd) {
+        this.kd = kd;
+    }
+}
+
+const bestFragger = json.players.reduce((players) => Math.max(players));
+
+console.log(bestFragger);
+
+
+console.log("\n" + "The following players are good at Rainbow Six Siege!");
 //Sort the players by kd and print
-const goodPlayers = json.players.filter(kd => kd >= 1.0);
+const goodPlayers = json.players.filter((players) => players.kd >= 1.0);
 
-for (x in goodPlayers){
-    console.log(x.name);
-}
-
-for (x in json.players){
-    console.log(x.kd);
-}
+goodPlayers.forEach(goodPlayers => console.log(goodPlayers.name));
 
 //Sort the players by platform and print
+const pcPlayers = json.players.filter((players) => players.platform === "PC");
 
+console.log("\n" + "The following players play on PC!");
+pcPlayers.forEach(pcPlayers => console.log(pcPlayers.name));
