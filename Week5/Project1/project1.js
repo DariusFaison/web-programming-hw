a05d62e631c7c94e72aa13df4386fa53e0a9309c
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2020-09-18 23:58:34 -0400

    Final version

--- Week5/Project1/project1.js
+++ Week5/Project1/project1.js
@@ -1,7 +1,6 @@
 const json = require('./siege.json') //Get the JSON file
 
-//Create a higher order function that takes in a sort function and iterates over the array
-
+//Creates a class type of player that holds the name, platform, rank, and kd of one of the JSON objects
 class Player {
     constructor(name, platform, rank, kd) {
         this.name = name;
@@ -11,46 +10,62 @@ class Player {
     }
 
     get name() {
-        return this.name;
+        return this._name;
     };
 
     set name(name) {
-        this.name = name;
+        this._name = name;
     }
 
     get platform() {
-        return this.platform;
+        return this._platform;
     };
 
     set platform(platform) {
-        this.platform = platform;
+        this._platform = platform;
     };
 
     get rank() {
-        return this.rank;
+        return this._rank;
     };
 
     set rank(rank) {
-        this.rank = rank;
+        this._rank = rank;
     };
 
     get kd() {
-        return this.kd;
+        return this._kd;
     };
 
     set kd(kd) {
-        this.kd = kd;
+        this._kd = kd;
     }
 }
 
-const bestFragger = json.players.reduce((players) => Math.max(players));
+//Function to sort by the property of one the JSON objects
+function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] < b[property])  
+          return 1;  
+       else if(a[property] > b[property])  
+          return -1;  
+   
+       return 0;  
+    }  
+ }
 
-console.log(bestFragger);
+//Sort the array by highest to lowest kd and then assign the highest kd player to the Player variable bestFragger and print out their info
+const highestKD = json.players.sort(sortByProperty("kd"));
 
+let bestFragger = new Player(highestKD[0].name, highestKD[0].platform, highestKD[0].rank, highestKD[0].kd);
+
+console.log(bestFragger.name + " is the best fragger. They play on " + bestFragger.platform + " and their rank is " + bestFragger.rank + ". They have a KD of " + bestFragger.kd + "!");
+
+
+//Sort the players by kds higher than or equal to 1.0 and print
+const goodPlayers = json.players.filter((players) => players.kd >= 1.0);
 
 console.log("\n" + "The following players are good at Rainbow Six Siege!");
-//Sort the players by kd and print
-const goodPlayers = json.players.filter((players) => players.kd >= 1.0);
 
 goodPlayers.forEach(goodPlayers => console.log(goodPlayers.name));
 
@@ -58,4 +73,5 @@ goodPlayers.forEach(goodPlayers => console.log(goodPlayers.name));
 const pcPlayers = json.players.filter((players) => players.platform === "PC");
 
 console.log("\n" + "The following players play on PC!");
+
 pcPlayers.forEach(pcPlayers => console.log(pcPlayers.name));
