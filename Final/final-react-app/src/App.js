e5e51b0e3e68f15ed57b7f1575d70973c29fe2c0
Darius Faison <33269709+DariusFaison@users.noreply.github.com>
2020-11-12 05:23:28 -0500

    1 Hook Implemented
    Hook implemented that returns a fact from the random fact API

--- Final/final-react-app/src/App.js
+++ Final/final-react-app/src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [facts, setFacts] = useState(() => {
+    console.log("placeholder for facts");
+    return 0;
+  });
+
+  const [pics, setPics] = useState(() => {
+    console.log("placeholder for pics");
+    return 0;
+  })
+
+
+  function getFacts() {
+    axios.get('https://uselessfacts.jsph.pl/random.txt?language=en').then(res => {
+      setFacts(res.data);
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+  function getPics() {
+    console.log("I'm also returning something!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={getFacts}>New Fact</button>
+      <span>{facts}</span>
+      <button onClick={getPics}>New Pic</button>
+    </>
   );
 }
 
